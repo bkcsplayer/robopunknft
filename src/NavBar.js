4927b586b0f824089b07dd63a94b29c55f8854a1
Kuo-Cool <39246555+bkcsplayer@users.noreply.github.com>
2022-07-26 13:50:59 +0800

    Update NavBar.js

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Box, Button, Flex, Image, Link, Spacer } from '@chakra-ui/react';
 import Facebook from "./assets/social-media-icons/facebook_32x32.png";
-import Twitter from "./assets/social-media-icons/twitter_32x32.png";
+import Youtube from "./assets/social-media-icons/youtube_32x32.png";
 import Email from "./assets/social-media-icons/email_32x32.png";
 
 const NavBar = ({accounts, setAccounts}) => {
@@ -25,8 +25,8 @@ const NavBar = ({accounts, setAccounts}) => {
                 <Link href="https://www.facebook.com">
                     <Image src={Facebook} boxSize="42px" margin="0 15px"/>
                 </Link>
-                <Link href="https://www.twitter.com">
-                    <Image src={Twitter} boxSize="42px" margin="0 15px"/>
+                <Link href="https://www.youtube.com">
+                    <Image src={Youtube} boxSize="42px" margin="0 15px"/>
                 </Link>
                 <Link href="https://www.gmail.com">
                     <Image src={Email} boxSize="42px" margin="0 15px"/>
@@ -46,7 +46,7 @@ const NavBar = ({accounts, setAccounts}) => {
 
             {/*Connect*/}
             { isConnected ? (
-                <Box margin="0 15px">Connected</Box>
+                <Box margin="0 15px">We Connected</Box>
             ) : (
                 <Button 
                 backgroundColor="#008fd4"
